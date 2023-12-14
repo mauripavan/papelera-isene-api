8df5817ad4277f43741e35c89181acd0a30c3368
mauripavan <mauriciopavan60@gmail.com>
2023-12-14 12:13:16 -0300

    feat: cors middleware implementation

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 const routes = require('./routes');
 const db = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors({ origin: true, credentials: true }));
 app.use('/api', routes);
 
 db.sync({ force: false }).then(() => {
