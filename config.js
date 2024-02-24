b4f97f4bc997b4f4ae9e31c8606c7b401af2797d
mauripavan <mauriciopavan60@gmail.com>
2024-02-24 15:53:50 -0300

    fix: update con config file

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   DB_PORT: process.env.PORT || 8080,
-  DB_NAME: process.env.PORT || 'papaleradb',
-  DB_USER: process.env.PORT || null,
-  DB_PASSWORD: process.env.PORT || null,
-  DB_HOST: process.env.PORT || '127.0.0.1',
-  DB_HOSTNAME: process.env.DB_HOST || 'localhost',
-  FRONTEND_URL: process.env.PORT || true,
+  DB_NAME: process.env.DB_NAMEPORT || 'papaleradb',
+  DB_USER: process.env.DB_USER || null,
+  DB_PASSWORD: process.env.DB_PASSWORD || null,
+  DB_HOST: process.env.DB_HOST || '127.0.0.1',
+  DB_HOSTNAME: process.env.DB_HOSTNAME || 'localhost',
+  FRONTEND_URL: process.env.FRONTEND_URL || true,
   SSL: process.env.SSL || false,
 };
