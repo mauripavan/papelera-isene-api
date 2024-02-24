c58317cc6938b7d9a5a44f100e8acd5d3982f7dd
mauripavan <mauriciopavan60@gmail.com>
2024-02-24 03:34:44 -0300

    fix: sequelize update

--- db/config/config.js
+++ db/config/config.js
@@ -5,14 +5,6 @@ const config = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     logging: false,
-    hostname: 'dpg-cncn5cacn0vc73f2u7n0-a',
-    host: '0.0.0.0',
-    user: 'mauricio',
-    password: 'M8kV8N0piPMjn17FzVl3ea7s7paEOhdn',
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
   },
 };
 module.exports = config;
