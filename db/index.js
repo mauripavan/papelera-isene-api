8775574931a415ea68f30aac8787df7d1071fd6b
mauripavan <mauriciopavan60@gmail.com>
2024-02-24 03:26:12 -0300

    fix: sequelize update

--- db/index.js
+++ db/index.js
@@ -1,17 +1,6 @@
 const Sequelize = require('sequelize');
-// const config = require('./config/config');
+const config = require('./config/config');
 
-const sequelize = new Sequelize({
-  database: 'papaleradb',
-  username: 'mauripavan',
-  password: 'M8kV8N0piPMjn17FzVl3ea7s7paEOhdn',
-  host: '0.0.0.0',
-  hostname: 'dpg-cncn5cacn0vc73f2u7n0-a',
-  port: 5432,
-  dialect: 'postgres',
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const sequelize = new Sequelize(config.production);
 
 module.exports = sequelize;
