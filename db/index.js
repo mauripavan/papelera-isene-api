3bd6472ff85d3c9af9c3f72f05388459ee3ba2eb
mauripavan <mauriciopavan60@gmail.com>
2024-02-24 02:20:22 -0300

    fix: sequelize update

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const config = require('./config/config');
+// const config = require('./config/config');
 
-const sequelize = new Sequelize(config.production);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
