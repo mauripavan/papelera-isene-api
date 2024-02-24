6071f049d8738002c253a883e814832cf16eb850
mauripavan <mauriciopavan60@gmail.com>
2024-02-24 04:40:48 -0300

    fix: sequelize update

--- db/index.js
+++ db/index.js
@@ -1,14 +1,13 @@
 const Sequelize = require('sequelize');
-// const config = require('./config/config');
 
 const sequelize = new Sequelize(
-  'papaleradb',
-  'mauripavan',
-  'M8kV8N0piPMjn17FzVl3ea7s7paEOhdn',
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
   {
     dialect: 'postgres',
     logging: false,
-    host: 'dpg-cncn5cacn0vc73f2u7n0-a.oregon-postgres.render.com',
+    host: process.env.DB_HOST,
     dialectOptions: {
       ssl: true,
       rejectUnauthorized: false,
