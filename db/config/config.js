462610dacf284035705d7194bc104fe89bd5cebd
mauripavan <mauriciopavan60@gmail.com>
2024-02-24 04:12:48 -0300

    fix: sequelize update

--- db/config/config.js
+++ db/config/config.js
@@ -5,6 +5,8 @@ const config = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     logging: false,
+    host: '0.0.0.0',
+    hostname: 'dpg-cncn5cacn0vc73f2u7n0-a.oregon-postgres.render.com',
   },
 };
 module.exports = config;
