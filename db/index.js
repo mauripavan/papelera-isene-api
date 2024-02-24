80dff775a6f9677223a31c802244fa702ba05fb6
mauripavan <mauriciopavan60@gmail.com>
2024-02-24 01:48:48 -0300

    fix: sequelize update

--- db/index.js
+++ db/index.js
@@ -1,6 +1,17 @@
 const Sequelize = require('sequelize');
-const config = require('./config/config');
+// const config = require('./config/config');
+require('dotenv').config();
 
-const sequelize = new Sequelize(config.production);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    hostname: process.env.DB_HOSTNAME,
+  },
+);
 
 module.exports = sequelize;
