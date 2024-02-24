59d9d772594dcc880b47f7f5090d49e0495be1cb
mauripavan <mauriciopavan60@gmail.com>
2024-02-24 03:05:01 -0300

    fix: sequelize update

--- db/index.js
+++ db/index.js
@@ -5,7 +5,8 @@ const sequelize = new Sequelize({
   database: 'papaleradb',
   username: 'mauripavan',
   password: 'M8kV8N0piPMjn17FzVl3ea7s7paEOhdn',
-  hostname: 'pg-cncn5cacn0vc73f2u7n0-a',
+  host: '0.0.0.0',
+  hostname: 'dpg-cncn5cacn0vc73f2u7n0-a',
   port: 5432,
   dialect: 'postgres',
   ssl: {
